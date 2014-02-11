5d8556c5a8becdf0bcb6041ad8f5a7809636a414
patrick <pemmerich@intstrux.com>
2014-02-11 18:22:38 -0500

    first version functioning

--- js/Bubble.js
+++ js/Bubble.js
@@ -7,26 +7,30 @@ var p = Bubble.prototype = new createjs.Container(); // inherit from Container
 p.background;
 p.count = 0;
 p.bubbleTypes = ["red","blue","yellow","green"];
-p.bubbleType;
+p.type;
 p.radius = 15;
+p.row;
+p.column;
 
 p.Container_initialize = p.initialize;
-p.initialize = function(radius) {
+p.initialize = function(radius) 
+{
 	this.Container_initialize();
 	
 	if (radius)
 		this.radius = radius;
 	
-	this.bubbleType = this.bubbleTypes[Math.floor(Math.random() * this.bubbleTypes.length)];
+	this.type = this.bubbleTypes[Math.floor(Math.random() * this.bubbleTypes.length)];
 	
-	if(this.bubbleType == "red")
+	if(this.type == "red")
 		color = "#ff0000";
-	else if(this.bubbleType == "blue")
+	else if(this.type == "blue")
 		color = "#3366ff";
-	else if(this.bubbleType == "yellow")
+	else if(this.type == "yellow")
 		color = "#ffff33";
-	else if(this.bubbleType == "green")
+	else if(this.type == "green")
 		color = "#339933";
+	
 	
 	this.background = new createjs.Shape();
 	this.background.graphics.beginFill(color).drawCircle(0, 0, this.radius);
@@ -34,20 +38,43 @@ p.initialize = function(radius) {
 	this.setBounds(0,0,this.radius*2,this.radius*2);
 	
 	this.on("click", this.handleClick);
-	this.on("tick", this.handleTick);
+	//this.on("tick", this.handleTick);
 
 	this.addChild(this.background); 
 	this.mouseChildren = false;
 } 
 
-p.handleClick = function (event) {    
+p.handleClick = function (event) 
+{    
 	var target = event.target;
-	alert("You clicked on a button: "+ target.label);
+	smashBubble(this);
 } 
 
-p.handleTick = function(event) {   
+p.handleTick = function(event) 
+{   
 
-	//p.alpha = Math.cos(p.count++*0.1)*0.4+0.6;
+
+}
+
+p.updateBubble = function(type)
+{
+	this.type = type;
+	
+	if(this.type == "red")
+		color = "#ff0000";
+	else if(this.type == "blue")
+		color = "#3366ff";
+	else if(this.type == "yellow")
+		color = "#ffff33";
+	else if(this.type == "green")
+		color = "#339933";
+	
+	this.background = new createjs.Shape();
+	this.background.graphics.beginFill(color).drawCircle(0, 0, this.radius);
+	//must explicitly set bounds
+	this.setBounds(0,0,this.radius*2,this.radius*2);
+	this.addChild(this.background); 
+
 }
 
 window.Bubble = Bubble;
--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,5 @@
 var stage, holder;
-
+var smashTimer;
 
 function init() 
 {
@@ -10,6 +10,9 @@ function init()
 	for (var i=0; i<11; i++){
 		for (var n=0; n<10; n++){
 			var bubble = new Bubble(20);
+			bubble.row=i;
+			bubble.column=n;
+			bubble.name = "bubble_"+n+"_"+i;
 			stage.addChild(bubble);
 			bubble.x = (n*bubble.radius*2)+bubble.radius;
 			bubble.y = (i*bubble.radius*2)+bubble.radius+6; 
@@ -19,3 +22,70 @@ function init()
 	createjs.Ticker.on("tick", stage);
 }
 
+function smashBubble(bubble)
+{
+	window.clearTimeout(smashTimer);
+	smashTimer = setTimeout(cleanUpBubbles, 100);
+	console.log("name = "+bubble.name);
+	bubble.removeChild(bubble.background);
+	var row = bubble.row;
+	var column = bubble.column;
+	var type = bubble.type;
+	var count = 0;
+	
+	bubble.type = "blank";
+	//check surrounding bubbles
+	//up
+	var up = stage.getChildByName("bubble_"+column+"_"+(row-1));
+	try{
+		if(up.type == type){
+			smashBubble(up);
+		}
+	}catch(e){}
+	
+	//down
+	var down = stage.getChildByName("bubble_"+column+"_"+(row+1));
+	try{
+		if(down.type == type){
+			smashBubble(down);
+		}
+	}catch(e){};
+	
+	//left
+	var left = stage.getChildByName("bubble_"+(column-1)+"_"+row);
+	try{
+		if(left.type == type){
+			smashBubble(left);
+		}
+	}catch(e){}
+	
+	//right
+	var right = stage.getChildByName("bubble_"+(column+1)+"_"+row);
+	try{
+		if(right.type == type){
+			smashBubble(right);
+		}
+	}catch(e){}
+}
+
+function cleanUpBubbles()
+{
+	console.log("clean up bubbles");
+	for(var q=0; q<11; q++){
+		for (var i=0; i<11; i++){
+			for (var n=0; n<10; n++){
+				var bubble = stage.getChildByName("bubble_"+n+"_"+i);
+				if(bubble.type != "blank"){
+					var down = stage.getChildByName("bubble_"+n+"_"+(i+1));
+					try{
+						if(down.type == "blank"){
+							down.updateBubble(bubble.type);
+							bubble.type="blank";
+							bubble.removeChild(bubble.background);
+						}
+					}catch(e){};
+				}
+			}
+		}
+	}
+}
